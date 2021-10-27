dcdbf17d8456b1da87989307a60d601cbe85d5d4
Ran <adobe_vip@yahoo.com>
2021-10-27 21:31:51 +0800

    Update Always if.v

--- Verilog Language/Procedures/Always if.v
+++ Verilog Language/Procedures/Always if.v
@@ -1,16 +1,19 @@
 // synthesis verilog_input_version verilog_2001
 module top_module(
-    input clk,
     input a,
     input b,
+    input sel_b1,
+    input sel_b2,
     output wire out_assign,
-    output reg out_always_comb,
-    output reg out_always_ff   );
-assign out_assign = a ^ b;
+    output reg out_always   ); 
+assign out_assign = (sel_b1 & sel_b2) ? b : a;
+
 always @(*) begin
-	out_always_comb = a ^ b;
-end
-always @(posedge clk) begin
-	out_always_ff <= a ^ b;
-end
+	if(sel_b1 & sel_b2) begin
+		out_always = b;
+	end
+	else begin
+		out_always = a;
+	end
+end 
 endmodule
